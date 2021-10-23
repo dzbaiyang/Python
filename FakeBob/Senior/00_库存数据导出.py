c5bf32943cf760f805a13efa2405ba72ae48ab8c
FakeBob <dzbaiyang@live.cn>
2021-10-23 21:12:50 +0800

    2021023

--- FakeBob/Senior/00_库存数据导出.py
+++ FakeBob/Senior/00_库存数据导出.py
@@ -1,7 +1,32 @@
-
+import datetime
+import os
+import zipfile
 import pandas as pd
 import pymysql
+from openpyxl.packaging.manifest import mimetypes
+import smtplib
+from email.header import Header
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
 
+
+#获取时间变量#
+localtime = (datetime.datetime.now()-datetime.timedelta(days=1)).strftime("%Y%m%d") #多加（减）一天
+#获取路径变量
+pathname = 'D:\{}_库存明细数据.xlsx'.format(localtime)
+# page = '/data/mail_data/stock/{}_库存明细数据.xlsx'.format(localtime)
+
+# 获取压缩文件变量
+zipfilepath = 'D:\{}_库存明细数据.zip'.format(localtime)
+
+
+# 获取文件变量
+filename = '{}_库存明细数据'.format(localtime)
+
+
+#连接mysql数据库
 conn = pymysql.connect(
     host='offline-tech.ikunchi.com',
     user = "bi_pro",
@@ -11,9 +36,11 @@ conn = pymysql.connect(
     charset='utf8'
 )
 
+print("执行SQL时间：",datetime.datetime.now())
 df = pd.read_sql("""
 select ds as 库存时间,
 warehouse_type as 仓库属性,
+warehouse_is_proxy as 是否代销,
 org_code as 库存组织,
 org_name as 库存组织名称, 
 core_business_name as OBC仓库映射事业部,
@@ -45,7 +72,64 @@ stock_age as 库龄分布,
 with_amount as 计提金额,
 diff_invalid_age as 剩余效期,
 effective_type as 效期类型 from dm.dm_t08_01_stock_final a 
-where a.ds = '20210822'
+where ds = date_format(DATE_SUB(NOW(),INTERVAL 1 day),'%Y%m%d')
 """,con=conn)
-df.to_excel("E:\2021年8月15日库龄明细数据",index = False)
+print("执行SQL结束时间：",datetime.datetime.now())
+
+#####################################################################################################################################
+#输出文件内容
+page = pathname
+df.to_excel(page,index = False)
+
+# 压缩
+zip_file = zipfile.ZipFile(zipfilepath, 'w',zipfile.ZIP_DEFLATED)
+zip_file.write(page)
+zip_file.close()
+
+
+#邮件附件内容
+From = "bi-report@ilarge.cn"
+To = "baiyang@ilarge.cn"
+file_name = zipfilepath  #附件名
+
+server = smtplib.SMTP("smtp.exmail.qq.com")
+server.login("bi-report@ilarge.cn","f4ND674F9jGnGLhC") #仅smtp服务器需要验证时
+
+# 构造MIMEMultipart对象做为根容器
+main_msg = MIMEMultipart()
+
+# 构造MIMEText对象做为邮件显示内容并附加到根容器
+text_msg = MIMEText("Dear all：\n \n  附件为截止{}库存明细数据,请查收!".format(localtime)) #邮件正文内容
+main_msg.attach(text_msg)
+
+
+# 构造MIMEBase对象做为文件附件内容并附加到根容器
+ctype,encoding = mimetypes.guess_type(file_name)
+if ctype is None or encoding is not None:
+    ctype='application/octet-stream'
+maintype,subtype = ctype.split('/',1)
+file_msg=MIMEImage(open(file_name,'rb').read(),subtype)
+print(ctype,encoding)
+
+## 设置附件头
+basename = os.path.basename(file_name)
+file_msg.add_header('Content-Disposition','attachment', filename = basename)#修改邮件头
+main_msg.attach(file_msg)
+
+# 设置根容器属性
+main_msg['From'] = From
+main_msg['To'] = To
+main_msg['Subject'] = filename
+
+
+# 得到格式化后的完整文本
+fullText = main_msg.as_string( )
+
+# 用smtp发送邮件
+try:
+    server.sendmail(From, To, fullText)
+finally:
+    server.quit()
+
+
 
