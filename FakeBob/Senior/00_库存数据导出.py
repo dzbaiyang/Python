09de5c2cf8c622d335a8fafdb93cba46b0ba8211
FakeBob <dzbaiyang@live.cn>
2021-10-24 19:21:37 +0800

    2021024

--- FakeBob/Senior/00_库存数据导出.py
+++ FakeBob/Senior/00_库存数据导出.py
@@ -2,6 +2,7 @@ import datetime
 import os
 import zipfile
 import pandas as pd
+import sqlite3
 import pymysql
 from openpyxl.packaging.manifest import mimetypes
 import smtplib
@@ -15,12 +16,14 @@ from email.mime.application import MIMEApplication
 #获取时间变量#
 localtime = (datetime.datetime.now()-datetime.timedelta(days=1)).strftime("%Y%m%d") #多加（减）一天
 #获取路径变量
-pathname = 'D:\{}_库存明细数据.xlsx'.format(localtime)
-# page = '/data/mail_data/stock/{}_库存明细数据.xlsx'.format(localtime)
-
-# 获取压缩文件变量
-zipfilepath = 'D:\{}_库存明细数据.zip'.format(localtime)
-
+#测试
+# pathname = 'D:\Stock\{}_库存明细数据.xlsx'.format(localtime)
+# pathname1 = 'D:\Stock\库存{}分析报告.xlsx'.format(localtime)
+# zipfilepath = 'D:\Stock\{}_库存明细及库存分析报告.zip'.format(localtime)
+#服务器
+pathname = '/data/mail_data/stock/{}_库存明细数据.xlsx'.format(localtime)
+pathname1 = '/data/mail_data/stock/库存{}分析报告.xlsx'.format(localtime)
+zipfilepath = '/data/mail_data/stock/{}_库存明细及库存分析报告.zip'.format(localtime)
 
 # 获取文件变量
 filename = '{}_库存明细数据'.format(localtime)
@@ -35,9 +38,9 @@ conn = pymysql.connect(
     db = 'ads',
     charset='utf8'
 )
-
+#####################################################################################################################################
 print("执行SQL时间：",datetime.datetime.now())
-df = pd.read_sql("""
+stockdetail = pd.read_sql("""
 select ds as 库存时间,
 warehouse_type as 仓库属性,
 warehouse_is_proxy as 是否代销,
@@ -74,34 +77,45 @@ diff_invalid_age as 剩余效期,
 effective_type as 效期类型 from dm.dm_t08_01_stock_final a 
 where ds = date_format(DATE_SUB(NOW(),INTERVAL 1 day),'%Y%m%d')
 """,con=conn)
-print("执行SQL结束时间：",datetime.datetime.now())
-
+#####################################################################################################################################
+stockanalytics = pd.read_sql("""
+select * from ads.ads_t08_01_stock_daily_analytics
+""",con=conn)
 #####################################################################################################################################
 #输出文件内容
 page = pathname
-df.to_excel(page,index = False)
+page1 = pathname1
+stockdetail.to_excel(page,index = False)
+stockanalytics.to_excel(page1,index = False)
+print("执行SQL结束时间：",datetime.datetime.now())
+#####################################################################################################################################
+# 压缩目录下面的文件
+def compress_attaches(files, out_name):
+    f = zipfile.ZipFile(out_name, 'w', zipfile.ZIP_DEFLATED)
+    for file in files:
+        f.write(file)
+    f.close()
+files = [page, page1]
+compress_attaches(files, zipfilepath)
 
-# 压缩
-zip_file = zipfile.ZipFile(zipfilepath, 'w',zipfile.ZIP_DEFLATED)
-zip_file.write(page)
-zip_file.close()
-
-
+# zip_file = zipfile.ZipFile(zipfilepath, 'w',zipfile.ZIP_DEFLATED)
+# zip_file.write(page)
+# zip_file.close()
+#####################################################################################################################################
 #邮件附件内容
 From = "bi-report@ilarge.cn"
 To = "baiyang@ilarge.cn"
 file_name = zipfilepath  #附件名
 
-server = smtplib.SMTP("smtp.exmail.qq.com")
+server = smtplib.SMTP_SSL("smtp.exmail.qq.com",465)
 server.login("bi-report@ilarge.cn","f4ND674F9jGnGLhC") #仅smtp服务器需要验证时
 
 # 构造MIMEMultipart对象做为根容器
 main_msg = MIMEMultipart()
 
 # 构造MIMEText对象做为邮件显示内容并附加到根容器
-text_msg = MIMEText("Dear all：\n \n  附件为截止{}库存明细数据,请查收!".format(localtime)) #邮件正文内容
+text_msg = MIMEText("Dear all：\n \n  附件为截止{}库存明细数据及库存分析报告,请查收!\n \n  另事业部缺失信息，已在KATA系统中更新，还请及时维护，谢谢！\n \n  维护地址：https://bi.ikunchi.com/decision".format(localtime)) #邮件正文内容
 main_msg.attach(text_msg)
-
 
 # 构造MIMEBase对象做为文件附件内容并附加到根容器
 ctype,encoding = mimetypes.guess_type(file_name)
@@ -110,7 +124,6 @@ if ctype is None or encoding is not None:
 maintype,subtype = ctype.split('/',1)
 file_msg=MIMEImage(open(file_name,'rb').read(),subtype)
 print(ctype,encoding)
-
 ## 设置附件头
 basename = os.path.basename(file_name)
 file_msg.add_header('Content-Disposition','attachment', filename = basename)#修改邮件头
@@ -121,7 +134,6 @@ main_msg['From'] = From
 main_msg['To'] = To
 main_msg['Subject'] = filename
 
-
 # 得到格式化后的完整文本
 fullText = main_msg.as_string( )
 
@@ -130,6 +142,3 @@ try:
     server.sendmail(From, To, fullText)
 finally:
     server.quit()
-
-
-
