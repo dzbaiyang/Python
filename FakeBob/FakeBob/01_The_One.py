1588d3bc1b17bf7691a9118e0a27b106c3e9b90f
FakeBob <dzbaiyang@hotmail.com>
2022-01-23 22:23:22 +0800

    20220123

--- FakeBob/FakeBob/01_The_One.py
+++ FakeBob/FakeBob/01_The_One.py
@@ -1,2 +1,18 @@
-def func01():
-    print("Hello function")
+import random
+
+# def func01():
+#     for i in shuffle1:
+#         if i
+
+
+
+card_list = ["梅花","黑桃","方块","红心"]
+card_number = ["A",2,3,4,5,6,7,8,9,10,"J","Q","K"]
+all_card_list = [[color,num] for color in card_list for num in card_number]
+
+shuffle = random.shuffle(all_card_list)
+
+shuffle1 = all_card_list[0],all_card_list[1],all_card_list[2]
+
+
+
